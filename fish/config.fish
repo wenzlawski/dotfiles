6f9dca6f49ca2a376a0d4c08c92b1a3e3cdf6b18
Marc Wenzlawski <mw@Marcs-MacBook-Pro.local>
2023-03-13 13:30:48 +0000

    initial dots

---
+++ fish/config.fish
@@ -0,0 +1,45 @@
+# Aliases
+alias l=lsd
+alias la="lsd -a"
+alias lh="lsd -hla"
+alias sshpi="ssh pi@raspberrypi.local"
+alias jpn="jupyter notebook"
+alias jpl="jupyter lab"
+alias pjl="julia -e 'import Pluto; Pluto.run()'"
+alias fpublish='julia -e "using Franklin; publish()"'
+alias fserve='julia -e "using Franklin; serve()"'
+alias n=nvim
+alias newmac="sudo /sbin/ifconfig en0 ether \`openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.\$//'\`"
+alias pn=pnpm
+alias c=clear
+alias typing-practice="open https://climech.github.io/typing-practice/"
+alias github="open https://github.com"
+alias git-repos="open 'https://github.com/wenzlawski?tab=repositories'"
+alias git-me="open 'https://github.com/wenzlawski'"
+alias sb="search bing"
+alias sg="search google"
+
+set -U fish_greeting
+set -gx ATUIN_NOBIND "true"
+
+# Environment variables
+set -gx XDG_CONFIG_HOME ~/.config
+set -x PATH $PATH ~/.cargo/bin
+
+
+# CLI inits
+zoxide init fish | source
+atuin init fish | source
+starship init fish | source
+pyenv init - | source
+
+# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
+bind \cr _atuin_search
+bind -M insert \cr _atuin_search
+
+# pnpm
+set -gx PNPM_HOME "/Users/mw/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
---
+++ yarn/global/node_modules/create-tauri-app/build.rs
@@ -0,0 +1,9 @@
+// Copyright 2019-2022 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+extern crate napi_build;
+
+fn main() {
+    napi_build::setup();
+}
