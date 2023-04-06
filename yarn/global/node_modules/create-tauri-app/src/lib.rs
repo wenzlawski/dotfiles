08ee95f03ab7a41414c7bf444ab7173f30b5d62e
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-04-06 17:11:10 +0100

    remove useless configs

--- yarn/global/node_modules/create-tauri-app/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2022 Tauri Programme within The Commons Conservancy
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-License-Identifier: MIT
-
-#[allow(unused)]
-#[napi_derive::napi]
-fn run(args: Vec<String>, bin_name: Option<String>) {
-    create_tauri_app::run(args, bin_name);
-}
