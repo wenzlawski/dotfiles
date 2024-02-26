d5039e099520a1ee4aed5e1669a768292c55502f
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2024-02-26 13:00:31 +0100

    fix backwards dark mode adjustment

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -47,12 +47,14 @@ function autoSetWallpaper()
 		hs.osascript.applescript('tell application "System Events" to tell appearance preferences to get dark mode')
 		== "true"
 	then
-		hs.execute(
-			'osascript -e \'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Silver.png" as POSIX file\''
-		)
-	else
+		logger.d("dark mode")
 		hs.execute(
 			'osascript -e \'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Black.png" as POSIX file\''
+		)
+	else
+		logger.d("light mode")
+		hs.execute(
+			'osascript -e \'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Silver.png" as POSIX file\''
 		)
 	end
 end
