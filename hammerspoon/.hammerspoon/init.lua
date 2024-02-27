96ec43f4eab9342ec40c654eb5557df229cff807
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2024-02-27 18:43:30 +0100

    FINAL fix to autoSetWallpaper

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -44,8 +44,8 @@ end)
 
 function autoSetWallpaper()
 	local _, darkmode =
-		hs.osascript.applescript('tell application "System Events" to tell appearance preferences to get dark mode')
-	if darkmode == "true" then
+		hs.execute("osascript -e 'tell application \"System Events\" to tell appearance preferences to get dark mode'")
+	if darkmode == true then
 		logger.d("dark mode")
 		hs.execute(
 			'osascript -e \'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Black.png" as POSIX file\''
