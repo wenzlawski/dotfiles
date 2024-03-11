c9dfbd2325afa9a66dd782a816d245119053771d
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2024-03-11 14:23:40 +0100

    add vterm functions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -34,6 +34,29 @@ set -x PATH $PATH /usr/local/Cellar/recoll/1.35.0/recoll.app/Contents/MacOS/
 set -x FZF_DEFAULT_COMMAND 'fd --type file --follow --hidden --exclude .git --color=always'
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+function vterm_prompt_end;
+    vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+end
+functions --copy fish_prompt vterm_old_fish_prompt
+function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+    # Remove the trailing newline from the original prompt. This is done
+    # using the string builtin from fish, but to make sure any escape codes
+    # are correctly interpreted, use %b for printf.
+    printf "%b" (string join "\n" (vterm_old_fish_prompt))
+    vterm_prompt_end
+end
 
 #pyenv virtualenv-init - | source
 
