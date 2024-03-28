a1be1c2616b76623a9eee08c948c5e65c06527d1
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2024-03-28 14:34:18 +0100

    clean up config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,9 @@
 # Base16 Shell
 if status --is-interactive
     # CLI inits
-    #set -gx ATUIN_NOBIND true
     zoxide init fish | source
     #atuin init fish | source
-    #starship init fish | source
     pyenv init - | source
-    # set BASE16_SHELL_PATH "$HOME/.config/base16-shell"
-    # set BASE16_SHELL "$HOME/.config/base16-shell"
-    # if test -s "$BASE16_SHELL_PATH" -a "$TERM" = "screen-256color"
-    # if test -s "$BASE16_SHELL_PATH"
-    #   source "$BASE16_SHELL_PATH/profile_helper.fish"
-    # end
 end
 
 # Environment variables
@@ -30,7 +22,7 @@ fish_add_path ~/.config/emacs/bin
 fish_add_path ~/.local/bin
 #fish_add_path ~/.local/lib/python3.10/site-packages
 fish_add_path $GOHOME/bin
-fish_add_path /usr/local/Cellar/recoll/1.35.0/recoll.app/Contents/MacOS/
-fish_add_path "$VOLTA_HOME/bin"
+fish_add_path /usr/local/Cellar/recoll/1.35.0/recoll.app/Contents/MacOS
+fish_add_path $VOLTA_HOME/bin
 fish_add_path $PNPM_HOME
-
+fish_add_path ~/.pyenv/shims
